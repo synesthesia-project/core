29856da038bba9aeb0b4d05124fbd06cf7996627
Sam Lanning <sam@samlanning.com>
2019-01-05 14:33:39 +0000

    move shared code to new core node module

---
+++ src/protocol/index.ts
@@ -0,0 +1,5 @@
+import * as messages from './messages';
+import {ConsumerEndpoint} from './consumer';
+import {ControllerEndpoint} from './controller';
+
+export {messages, ConsumerEndpoint, ControllerEndpoint};
