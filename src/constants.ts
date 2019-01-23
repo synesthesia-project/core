404d13a0518c1fd54501778a5cfadc00500aa711
Sam Lanning <sam@samlanning.com>
2019-01-22 17:48:04 -0800

    Add composer constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
 export const DEFAULT_SYNESTHESIA_PORT = 8120;
+
 export const SYNESTHESIA_WEBSOCKET_PATH = '/synesthesia';
 export const CONTROLLER_WEBSOCKET_PATH = '/control';
+
+export const COMPOSER_PATH = '/composer';
+export const DEFAULT_COMPOSER_URL = `http://localhost:${DEFAULT_SYNESTHESIA_PORT}${COMPOSER_PATH}`;
