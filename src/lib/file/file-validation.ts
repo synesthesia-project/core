959b49fdb81d77c2ed4a4285d4d68b7ff4cf0cd3
Sam Lanning <sam@samlanning.com>
2019-06-20 18:02:03 -0700

    Prepare package for inclusion in synesthesia monorepo

--- src/lib/file/file-validation.ts
+++ src/lib/file/file-validation.ts
@@ -1,5 +1,5 @@
 import * as util from '../util';
-import {CueFile} from './';
+import {CueFile} from '.';
 
 export function validateFile(obj: any) {
   // TODO: actually validate, for now assume it's fine
